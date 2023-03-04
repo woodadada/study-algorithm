8a3f348812fcde7030d7e274488442d4e45f708e
woodadada <60130985+woodadada@users.noreply.github.com>
2023-03-05 01:06:49 +0900

    [unrated] Title: 카드 뭉치, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int index1 = 0;
+        int index2 = 0;
+
+        for (int i = 0; i < goal.length; i++) {
+            String s = goal[i];
+            if(index1 < cards1.length && s.equals(cards1[index1])){
+                index1++;
+            }else if(index2 < cards2.length && s.equals(cards2[index2])){
+                index2++;
+            }else{
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}
