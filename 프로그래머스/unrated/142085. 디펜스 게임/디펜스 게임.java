919e67ce15fa70d244fec7fdd851a28ea6b03250
woodadada <60130985+woodadada@users.noreply.github.com>
2023-04-19 17:34:56 +0900

    [unrated] Title: 디펜스 게임, Time: 0.87 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for(int i = 0; i < enemy.length; i++) {
+            pq.add(enemy[i]);
+            
+            if(pq.size() > k)
+                n -= pq.poll();
+            
+            if(n < 0) {
+                return i;
+            }
+        }
+        
+        return enemy.length;
+    }
+}
