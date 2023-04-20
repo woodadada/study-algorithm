f0491ae0fee58f7ac73130ea97c37c4ed3830c72
woodadada <60130985+woodadada@users.noreply.github.com>
2023-04-21 00:15:06 +0900

    [unrated] Title: 달리기 경주, Time: 0.08 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+     
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i + 1);
+        }
+
+        for (String calling : callings) {
+            Integer index = map.get(calling);
+            map.put(calling, index - 1);
+
+            String player = players[index - 2];
+            map.put(player, map.get(player) + 1);
+            swap(players, index - 1);
+        }
+        return players;
+    }
+    
+    public void swap(String[] players, int index) {
+        String temp = players[index];
+        players[index] = players[index - 1];
+        players[index - 1] = temp;
+    }
+}
