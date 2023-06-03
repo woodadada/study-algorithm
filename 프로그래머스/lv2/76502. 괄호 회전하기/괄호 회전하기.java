792990978e8ed39a8cc6a4b95893c75014779939
woodadada <60130985+woodadada@users.noreply.github.com>
2023-06-03 20:36:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.56 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -31,6 +31,11 @@ class Solution {
                 }
             }
         }
+        
+        if(!stack.isEmpty()) {
+            return false;
+        }
+        
         return true;
     }
 }
