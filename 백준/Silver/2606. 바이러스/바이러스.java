c05fd29dd15b850273b278e71c920419bf31e0f9
woodadada <60130985+woodadada@users.noreply.github.com>
2024-03-29 17:27:56 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,43 +5,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static int[][] arr;
-    public static boolean[] visited;
-    public static int N, M;
-    public static Set<Integer> set = new TreeSet<>();
+    static int N, M;
+    static char[][] map;
+    static List<List<Integer>> list;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         //System.setIn(new FileInputStream("src/text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-
-        arr = new int[N + 1][N + 1];
         visited = new boolean[N + 1];
+        list = new ArrayList<>();
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // 인접 리스트 세팅
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+
+            list.get(a).add(b);
+            list.get(b).add(a);
         }
-
-        bfs(1);
-        System.out.println(set.size());
+        dfs();
     }
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
 
-        while(!queue.isEmpty()) {
-            Integer poll = queue.poll();
-            for (int i = 1; i <= N; i++) {
-                if(arr[poll][i] == 1 && !visited[i]) {
-                    queue.offer(i);
-                    visited[poll] = true;
-                    set.add(i);
+    public static void dfs() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        visited[1] = true;
+        int count = 0;
+
+        while(!stack.isEmpty()) {
+            Integer pop = stack.pop();
+
+            for (Integer node : list.get(pop)) {
+                if(!visited[node]) {
+                    stack.push(node);
+                    visited[node] = true;
+                    count++;
                 }
             }
         }
+        System.out.println(count);
     }
 }
