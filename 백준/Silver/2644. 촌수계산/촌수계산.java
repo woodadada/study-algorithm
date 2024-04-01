2ea9476cc8fd93d6e88eb1980bfd3b1fd68216f3
woodadada <60130985+woodadada@users.noreply.github.com>
2024-04-01 17:46:58 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -5,51 +5,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static int[][] arr;
-    public static boolean[] visited;
-    public static int[] depth;
-    public static int N, M, X, Y;
+    static int N, M, A, B;
+    static List<List<Integer>> list;
+    static boolean[] visited;
+    static int[] depth;
 
     public static void main(String[] args) throws IOException {
         //System.setIn(new FileInputStream("src/text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(br.readLine());
+        visited = new boolean[N + 1];
+        depth = new int[N + 1];
+        Arrays.fill(depth, -1);
+        list = new ArrayList<>();
 
-        arr = new int[N+1][N+1];
-        visited = new boolean[N+1];
-        depth = new int[N+1];
-        for (int i = 0; i < M; i++) { // 인접 행렬 초기화
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
         }
-        System.out.println(bfs(X));
+
+        // 인접 리스트 세팅
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.get(x).add(y);
+            list.get(y).add(x);
+        }
+        dfs();
+        System.out.println(depth[B]);
     }
-    public static int bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        visited[start] = true;
-        int count = 0;
 
-        while(!queue.isEmpty()) {
-            Integer poll = queue.poll();
-            if(poll == Y) break;
+    public static void dfs() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(A);
+        depth[A] = 0;
+        visited[A] = true;
 
-            for (int i = 1; i <= N; i++) {
-                if(arr[poll][i] == 1 && !visited[i]) {
-                    queue.offer(i);
+        while(!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            if(pop == B) break;
+
+            for (Integer i : list.get(pop)) {
+                if(!visited[i]) {
+                    stack.push(i);
+                    depth[i] = depth[pop] + 1;
                     visited[i] = true;
-                    depth[i] = depth[poll] + 1;
                 }
             }
         }
-
-        return depth[Y] == 0 ? -1 : depth[Y];
     }
 }
