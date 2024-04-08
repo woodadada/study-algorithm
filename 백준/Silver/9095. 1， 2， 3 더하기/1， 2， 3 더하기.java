14bbfc421dc29984a3a7faf565df1de6334019e1
woodadada <60130985+woodadada@users.noreply.github.com>
2024-04-08 14:55:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 128 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("src/text.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] dp = new int[11];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+
+            System.out.println(dp[n]);
+        }
+    }
+}
